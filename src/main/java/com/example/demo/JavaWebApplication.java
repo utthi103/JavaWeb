2dcf915ffb51f1743db6ba876122c258320afd72
utthi103 <truongthiutthi2003@gmail.com>
2023-10-31 21:55:26 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/JavaWebApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class JavaWebApplication implements CommandLineRunner {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWebApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		   String sql = "INSERT INTO admin ( admin_fullname,admin_username,admin_password,admin_email,admin_phone,admin_address  )"
+	        		+ " VALUES ( ?,?,?,?,?,?)";
+		   int result = jdbcTemplate.update(sql,"sd","z","ád","s","sa","d");
+		   if(result>0) {
+			   System.out.println("insert a row");
+		   }
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/Model/Employee.java
@@ -0,0 +1,55 @@
+package com.example.demo.Model;
+
+public class Employee {
+	private int admin_id;
+	private String admin_fullname;
+	private String admin_username;
+	private String admin_password;
+	private String admin_email;
+	private String admin_phone	;
+	private String admin_address;
+	public int getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(int admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getAdmin_fullname() {
+		return admin_fullname;
+	}
+	public void setAdmin_fullname(String admin_fullname) {
+		this.admin_fullname = admin_fullname;
+	}
+	public String getAdmin_username() {
+		return admin_username;
+	}
+	public void setAdmin_username(String admin_username) {
+		this.admin_username = admin_username;
+	}
+	public String getAdmin_password() {
+		return admin_password;
+	}
+	public void setAdmin_password(String admin_password) {
+		this.admin_password = admin_password;
+	}
+	public String getAdmin_email() {
+		return admin_email;
+	}
+	public void setAdmin_email(String admin_email) {
+		this.admin_email = admin_email;
+	}
+	public String getAdmin_phone() {
+		return admin_phone;
+	}
+	public void setAdmin_phone(String admin_phone) {
+		this.admin_phone = admin_phone;
+	}
+	public String getAdmin_address() {
+		return admin_address;
+	}
+	public void setAdmin_address(String admin_address) {
+		this.admin_address = admin_address;
+	}
+	
+	
+}
